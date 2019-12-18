41211bc884d6d82cf1753f8fc2e468745f0e24d2
Rungroj Sonsbuanan <rungroj_s@cmu.ac.th>
2019-12-18 17:41:50 +0700

    Add read controller and change some wording

--- controllers/person.controller.js
+++ controllers/person.controller.js
@@ -1,15 +1,22 @@
 const Person = require('../models/person.model');
 
 exports.create = function (req, res) {
-    let product = new Product({
+    let person = new Person({
         name: req.body.name,
         age: req.body.age
     });
 
-    product.save(function (err) {
+    person.save(function (err) {
         if (err) {
             return next(err);
         }
-        res.send('Produc Created Successfully');
+        res.send('Person Created Successfully');
+    })
+}
+
+exports.read = function (req, res) {
+    Person.findById(req.params.id, function (err, person) {
+
+        res.send(person);
     })
 }
